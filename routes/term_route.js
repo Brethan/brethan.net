cfc909f7cf7cbc1caf4db17d32f4d76bb5c74736
Brethan <ethanbradley2001@gmail.com>
2023-06-08 01:24:50 -0400

    Added the cache layer to the JSON api

--- routes/term_route.js
+++ routes/term_route.js
@@ -1,6 +1,20 @@
+require("dotenv").config()
 const express = require("express");
 const getCourseInfo = require("../functions/scheduleScraper");
 const router = express.Router()
+const { initializeApp } = require("firebase/app")
+const { getFirestore, setDoc, doc, getDoc } = require("firebase/firestore");
+
+const firebaseConfig = {
+	apiKey: process.env.API_KEY,
+	authDomain: `${process.env.FIREBASE_PROJECT_NAME}.firebaseapp.com`,
+	databaseURL: `https://${process.env.FIREBASE_PROJECT_NAME}.firebaseio.com`,
+	projectId: `${process.env.FIREBASE_PROJECT_NAME}`,
+	storageBucket: `${process.env.FIREBASE_PROJECT_NAME}.appspot.com`,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp)
 
 router.get("/", (req, res) => {
 	res.send("tf you doing here bruv")
@@ -12,14 +26,17 @@ router.get("/:semester/:year/courseCode/:department-:number", async (req, res) =
 			return res.status(400).send("<h1>No thanks</h1>")
 	}
 	
-	const { department, number, semester, year } = req.params;
+	const { number, year } = req.params;
+	const semester = req.params.semester.toUpperCase();
+	const department = req.params.department.toUpperCase();
+
 	/** @type {number} */
 	let mappedSem;
-	switch (semester.toLowerCase()) {
-		case "winter":
+	switch (semester) {
+		case "WINTER":
 			mappedSem = 10
 			break;
-		case "summer":
+		case "SUMMER":
 			mappedSem = 20
 			break;
 		default: // "FA" or any other weird thing
@@ -27,13 +44,41 @@ router.get("/:semester/:year/courseCode/:department-:number", async (req, res) =
 			break;
 	}
 
-	try { // TODO: Needs to check DB first before adding searching carleton
-		const data = await getCourseInfo(department.toUpperCase(), number, mappedSem, year);
-		res.json({data: data})
+	const dbCourseRoute = `${semester}${year}/${department}-${number}`;
+	console.time("Total Fetch Time")
+	try {
+		console.time("Cache Check Overhead")
+		const cache = await getDoc(doc(db, dbCourseRoute));
+		console.timeEnd("Cache Check Overhead")
+		//
+		let data;
+		if (cache.exists()) {
+			console.log("cache hit", dbCourseRoute);
+			data = cache.data();
+		} else {
+			// fetch from carleton website
+			console.time("Real Fetch Time")
+			data = await getCourseInfo(department, number, mappedSem, year);
+			if (data[0]?.crn) { // cache the results in the DB
+				console.log("cache miss", dbCourseRoute);
+				setDoc(doc(db, dbCourseRoute), { data: data })
+				.then(() => console.log(`done caching ${dbCourseRoute}\n`))
+				.catch(() => console.log(`could not cache ${dbCourseRoute}`));
+			}
+			console.timeEnd("Real Fetch Time")
+			
+			data = { data: data };
+		}
+		
+		const status = data.data[0]?.crn ? 200 : 404;
+		res.status(status).json(data)
 		
 	} catch (error) {
+		console.log("Something the bad happened.", error);
 		res.status(500).send("<h1>Server Error 500</h1>")
 	}
+	console.timeEnd("Total Fetch Time")
+	console.log();
 })
 
 module.exports = router
